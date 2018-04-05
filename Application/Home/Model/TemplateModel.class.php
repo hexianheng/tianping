7b0f7abf84305f7e16d520a78716cf4d71e633d3
wangpeiyun <wangpeiyun@behe.com>
2018-04-05 19:55:35 +0800

    wpy

--- Application/Home/Model/TemplateModel.class.php
+++ Application/Home/Model/TemplateModel.class.php
@@ -5,6 +5,7 @@
  * */
 namespace Home\Model;
 class TemplateModel extends BaseModel {
+    private $cid = 0;
     private $templateConf = [
         //用户导入
         'user' => [
@@ -19,11 +20,43 @@ class TemplateModel extends BaseModel {
         //检测结果导入
         'data' => [
             'code' => 'SNP位点'
+        ],
+        //产品上传
+        'product' => [
+            'name' => '产品名称',
+            'projectStr' => '检测项目',
+            'desc' => '产品介绍'
+        ],
+        //项目上传
+        'project' => [
+            'name' => '项目名称',
+            'text' => '项目介绍'
+        ],
+        //项目上传
+        'site' => [
+            'itemid' => '项目ID',
+            'gene' => '基因名称',
+            'origincode' => 'rs号',
+            'gene_text' => '描述',
+            'wild_type' => '野生型',
+            'mutant_type' => '突变型',
+            'genotype_value_ww' => 'ww基因型频率',
+            'genotype_value_wm' => 'wm基因型频率',
+            'genotype_value_mm' => 'mm基因型频率',
+            'risk_desc_ww' => 'ww描述',
+            'risk_desc_wm' => 'wm描述',
+            'risk_desc_mm' => 'mm描述',
+            'ww_text' => 'ww逻辑',
+            'wm_text' => 'wm逻辑',
+            'mm_text' => 'mm逻辑'
         ]
     ];
     private $templateName = [
         'user' => '用户上传模版',
         'data' => '检测结果导入模版',
+        'product' => '产品上传模版',
+        'project' => '项目上传模版',
+        'site' => '位点上传模版'
     ];
 
     //获取模版
@@ -36,6 +69,7 @@ class TemplateModel extends BaseModel {
 
     //文件上传
     public function fileUpload($data){
+        $this->cid = empty($data['cid']) ? 0 : $data['cid'];
         //验证模版类型
         if($data['type'] == '' || empty($this->templateConf[$data['type']])){
             return $this->returnMsg('A058');
@@ -97,8 +131,10 @@ class TemplateModel extends BaseModel {
         $res = [];
         $headerKey = array_keys($header);
         $headerVal = array_values($header);
+        $sqlHead = '';
         //定制处理
         switch ($type) {
+            //用户上传
             case 'user' :
                 //获取验证所需的code
                 $res = array_column($arr,0);
@@ -111,8 +147,46 @@ class TemplateModel extends BaseModel {
                     unset($result['sql']);
                     return $result;
                 }else{
+                    $sqlHead = "insert into customer (". implode(',',$headerKey) .",addtime) values ";
                     $result['tableName'] = 'customer';
                     $reData = array_column($reData,'code');
+                }
+                break;
+            //产品上传
+            case 'product' :
+                $res = array_column($arr,1);
+                $res = array_unique(explode('|',implode('|',$res)));
+                $sql = "select count(itemid) as itemNum from item where itemid in (". implode(',',$res) .")";
+                $re = $this->sqlQuery('item',$sql)[0]['itemNum'];
+                if(empty($re) || $re != count($res)){
+                    $result['errorData'] = $arr;
+                    $result['msg'] = '无匹配项目ID';
+                    unset($result['num']);
+                    unset($result['sql']);
+                    return $result;
+                }else{
+                    $sqlHead = "insert into product (`". implode('`,`',$headerKey) ."`,ctime,status,cId) values ";
+                    $result['tableName'] = 'product';
+                }
+                break;
+            //项目上传
+            case 'project' :
+                $sqlHead = "insert into item (`". implode('`,`',$headerKey) ."`,addtime) values ";
+                $result['tableName'] = 'item';
+                break;
+            case 'site' :
+                $res = array_unique(array_column($arr,0));
+                $sql = "select count(itemid) as itemNum from item where itemid in (". implode(',',$res) .")";
+                $re = $this->sqlQuery('item',$sql)[0]['itemNum'];
+                if(empty($re) || $re != count($res)){
+                    $result['errorData'] = $arr;
+                    $result['msg'] = '无匹配项目ID';
+                    unset($result['num']);
+                    unset($result['sql']);
+                    return $result;
+                }else{
+                    $sqlHead = "insert into item_locus_value (`". implode('`,`',$headerKey) ."`,addtime) values ";
+                    $result['tableName'] = 'product';
                 }
                 break;
         }
@@ -120,8 +194,21 @@ class TemplateModel extends BaseModel {
         foreach($arr as $key => $val){
             $tempRes = [];
             switch ($type) {
+                //用户上传
                 case 'user' :
                     $tempRes = $this->userData($val,$headerVal,$headerKey,$reData);
+                    break;
+                //产品上传
+                case 'product' :
+                    $tempRes = $this->productData($val,$headerVal,$headerKey);
+                    break;
+                //产品上传
+                case 'project' :
+                    $tempRes = $this->projectData($val,$headerVal,$headerKey);
+                    break;
+                //点位上传
+                case 'site' :
+                    $tempRes = $this->siteData($val,$headerVal,$headerKey);
                     break;
             }
             if($tempRes['code'] == 0){
@@ -133,7 +220,7 @@ class TemplateModel extends BaseModel {
         }
         //处理数据
         if($result['num'] != 0){
-            $sql = implode(';',$result['sql']);
+            $sql = $sqlHead . implode(',',$result['sql']);
             $this->sqlQuery($result['tableName'],$sql);
         }
         return ['num' => $result['num'],'errData' => $result['errorData']];
@@ -179,7 +266,133 @@ class TemplateModel extends BaseModel {
         }
         $data[7] = date('Y-m-d H:i:s');
         $result['code'] = 0;
-        $result['sql'] = "insert into customer (". implode(',',$headerKey) .",addtime) values ('". implode("','",$data) ."')";
+        $result['sql'] = "('". implode("','",$data) ."')";
+        return $result;
+    }
+
+    //产品上传处理
+    public function productData($data,$headerVal,$headerKey){
+        $result = [
+            'sqlHead' => '',
+            'sql' => [],
+            'code' => -1,
+            'errorData' => []
+        ];
+        //验证产品名称
+        if($data[0] == ''){
+            $result['errorData'][] = $headerVal[0]. ' (' . $data[0] . ') ERROR';
+            return $result;
+        }
+        //验证产品介绍
+        if($data[2] == ''){
+            $result['errorData'][] = $headerVal[2]. ' (' . $data[2] . ') ERROR';
+            return $result;
+        }
+        $data[3] = date('Y-m-d H:i:s');
+        $data[4] = 0;
+
+        $result['code'] = 0;
+        $result['sql'] = "('". implode("','",$data) ."',$this->cid)";
+        return $result;
+    }
+
+    //项目上传处理
+    public function projectData($data,$headerVal,$headerKey){
+        $result = [
+            'sqlHead' => '',
+            'sql' => [],
+            'code' => -1,
+            'errorData' => []
+        ];
+        //验证项目名称
+        if($data[0] == ''){
+            $result['errorData'][] = $headerVal[0]. ' (' . $data[0] . ') ERROR';
+            return $result;
+        }
+        //验证产品介绍
+        if($data[1] == ''){
+            $result['errorData'][] = $headerVal[1]. ' (' . $data[1] . ') ERROR';
+            return $result;
+        }
+        $data[2] = date('Y-m-d H:i:s');
+
+        $result['code'] = 0;
+        $result['sql'] = "('". implode("','",$data) ."')";
+        return $result;
+    }
+
+    //点位上传
+    public function siteData($data,$headerVal,$headerKey){
+        $result = [
+            'sqlHead' => '',
+            'sql' => [],
+            'code' => -1,
+            'errorData' => []
+        ];
+        //项目ID
+        if($data[0] == ''){
+            $result['errorData'][] = $headerVal[0]. ' (' . $data[0] . ') ERROR';
+            return $result;
+        }
+        //基因名称
+        if($data[1] == ''){
+            $result['errorData'][] = $headerVal[1]. ' (' . $data[1] . ') ERROR';
+            return $result;
+        }
+        //rs号
+        if($data[2] == ''){
+            $result['errorData'][] = $headerVal[2]. ' (' . $data[2] . ') ERROR';
+            return $result;
+        }
+        //描述
+        if($data[3] == ''){
+            $result['errorData'][] = $headerVal[3]. ' (' . $data[3] . ') ERROR';
+            return $result;
+        }
+        //野生型
+        if($data[4] == ''){
+            $result['errorData'][] = $headerVal[4]. ' (' . $data[4] . ') ERROR';
+            return $result;
+        }
+        //突变型
+        if($data[5] == ''){
+            $result['errorData'][] = $headerVal[5]. ' (' . $data[5] . ') ERROR';
+            return $result;
+        }
+        //ww基因型频率
+        if($data[6] == ''){
+            $result['errorData'][] = $headerVal[6]. ' (' . $data[6] . ') ERROR';
+            return $result;
+        }
+        //wm基因型频率
+        if($data[7] == ''){
+            $result['errorData'][] = $headerVal[7]. ' (' . $data[7] . ') ERROR';
+            return $result;
+        }
+        //mm基因型频率
+        if($data[8] == ''){
+            $result['errorData'][] = $headerVal[8]. ' (' . $data[8] . ') ERROR';
+            return $result;
+        }
+        //ww描述
+        if($data[9] == ''){
+            $result['errorData'][] = $headerVal[9]. ' (' . $data[9] . ') ERROR';
+            return $result;
+        }
+        //wm描述
+        if($data[10] == ''){
+            $result['errorData'][] = $headerVal[10]. ' (' . $data[10] . ') ERROR';
+            return $result;
+        }
+        //mm描述
+        if($data[11] == ''){
+            $result['errorData'][] = $headerVal[11]. ' (' . $data[11] . ') ERROR';
+            return $result;
+        }
+        $data[15] = date('Y-m-d H:i:s');
+
+        $result['code'] = 0;
+        $result['sql'] = "('". implode("','",$data) ."')";
         return $result;
     }
 
